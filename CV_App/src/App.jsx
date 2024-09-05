5f3e8de1aed1477b1c767e77342e921dd367a644
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-05 23:22:00 +0100

    link GeneralInfo to App component

--- CV_App/src/App.jsx
+++ CV_App/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import GeneralInfo from './components/GeneralInfo'
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+      <GeneralInfo />
     </>
   )
 }
