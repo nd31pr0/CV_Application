6c0dfe531c4b932696d1f9e0c5605a15c6a426df
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-05 23:21:27 +0100

    write generaInfo component

--- CV_App/src/components/GeneralInfo.jsx
+++ CV_App/src/components/GeneralInfo.jsx
@@ -1,5 +1,20 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 
 const GeneralInfo = () => {
     const [info, setInfo] = useState({name: "", email: "", phone: ""})
+    const handleChange = (e) => {
+       const {name, value} = e.target
+       setInfo({...info, [name]: value})
+    };
+
+    return (
+        <div>
+            <h2>GeneralInfo</h2>
+            <input type='text' name='name' placeholder='Name' onchange={handleChange}/>
+            <input type='email' name='email' placeholder='Email' onchange={handleChange} />
+            <input type='tel' name='phone' placeholder='Enter Phone Number' onchange={handleChange}/>
+        </div>
+
+    )
 }
+export default GeneralInfo;
