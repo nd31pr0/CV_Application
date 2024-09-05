d51330442deb5ed0dfdbab3e6a973823c108ebae
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-05 23:13:28 +0100

    remove unnecessary files

--- CV_App/src/App.jsx
+++ CV_App/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Hello</p>
     </>
   )
 }
---
+++ CV_App/src/components/GeneralInfo.jsx
@@ -0,0 +1,5 @@
+import React, {useState} from 'react';
+
+const GeneralInfo = () => {
+    const [info, setInfo] = useState({name: "", email: "", phone: ""})
+}
