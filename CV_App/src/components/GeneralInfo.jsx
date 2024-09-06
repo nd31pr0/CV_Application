a0aeb0517c6c6b5b838303a26570c33f7e31dc0c
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-06 11:46:38 +0100

    refactored generalInfo component

--- CV_App/src/components/GeneralInfo.jsx
+++ CV_App/src/components/GeneralInfo.jsx
@@ -1,21 +1,41 @@
 import {useState} from 'react';
+import PropTypes from 'prop-types';
 
-const GeneralInfo = () => {
-    const [info, setInfo] = useState({name: "", email: "", phone: ""})
+const GeneralInfo = ({generalInfo, setGeneralInfo}) => {
+    const [isEditing, setIsEditing] = useState(false)
     const handleChange = (e) => {
        const {name, value} = e.target
-       setInfo({...info, [name]: value})
+       setGeneralInfo({...generalInfo, [name]: value})
     };
 
     return (
         <>
             <h2>GeneralInfo section</h2>
-            <input type='text' name='name' placeholder='Name' onchange={handleChange}/>
-            <input type='email' name='email' placeholder='Email' onchange={handleChange} />
-            <input type='tel' name='phone' placeholder='Enter Phone Number' onchange={handleChange}/>
-            <button type='submit'>submit</button>
+            {isEditing? (
+                <>
+                    <input type='text' name='name' value={generalInfo.name} onChange={handleChange} placeholder='name'/>
+                    <input type='text' name='email' value={generalInfo.email} onChange={handleChange} placeholder='email'/>
+                    <input type='tel' name='phone' value={generalInfo.phone} onChange={handleChange} placeholder='phone'/>
+                    <button onClick={()=>setIsEditing(false)}>Submit</button>
+                </>
+            ):(
+                <>
+                    <p>Name: {generalInfo.name}</p>
+                    <p>Email: {generalInfo.email}</p>
+                    <p>Phone: {generalInfo.phone}</p>
+                    <button onClick={()=>setIsEditing(true)}>Edit</button>
+                </>
+            )}
+            
         </>
 
     )
 }
+GeneralInfo.propTypes = {
+    generalInfo: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        phone: PropTypes.string.isRequired,
+    }).isRequired,
+    setGeneralInfo: PropTypes.func.isRequired,};
 export default GeneralInfo;
