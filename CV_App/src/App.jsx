bf7b83e50dfe076f06aa368d006f826a20b81d3d
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-06 11:47:47 +0100

    refactor interaction of GeneralInfo.jsx with App.jsx

--- CV_App/src/App.jsx
+++ CV_App/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
+import {useState} from 'react'
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education.jsx';
 import Practical from './components/Practical.jsx';
 function App() {
-  // const [count, setCount] = useState(0)
 
+  const [generalInfo, setGeneralInfo] = useState({name:'', email:'', phone:''})
   return (
     <>
-
+      <h1>CV Builder</h1>
       <Education />
-      <GeneralInfo />
+      <GeneralInfo generalInfo={generalInfo} setGeneralInfo={setGeneralInfo} />
+      
+      <h2>Education Section</h2>
       <Practical />
     </>
   )
